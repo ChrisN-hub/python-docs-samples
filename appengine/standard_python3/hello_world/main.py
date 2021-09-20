42d56fcb3fde7fa1be02248082d816a46ab29739
Chris Neubauer <chris.neubauer.395@hotmail.com>
2021-09-20 10:55:00 -0700

    Edit hello_world for lab 1

--- appengine/standard_python3/hello_world/main.py
+++ appengine/standard_python3/hello_world/main.py
@@ -25,7 +25,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+    return 'Hello \n Chris Neubauer \n 300287838!'
 
 
 if __name__ == '__main__':
